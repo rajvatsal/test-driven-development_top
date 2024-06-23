d528ff6c7cbc518563582c24f1a6af9fb8710775
Vatsal <rajvatsal2004@gmail.com>
2024-06-23 12:00:55 +0530

    feat(index): Add caesar's cipher

--- src/index.js
+++ src/index.js
@@ -1,4 +1,47 @@
 import "./css/styles.css";
-export default function getName() {
-	return "vatsal";
+export function capitalize(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function reverseString(str) {
+	return str.split("").reverse().join("");
+}
+
+export function Calculator() {
+	return {
+		add: (x, y) => x + y,
+		sub: (x, y) => x - y,
+		mul: (x, y) => x * y,
+		div: (x, y) => x / y,
+	};
+}
+
+function cipherLowerCase(char, shift) {
+	let cipher = char.charCodeAt() + shift;
+	while (cipher > 122) {
+		cipher = cipher - 123 + 97;
+	}
+	return String.fromCharCode(cipher);
+}
+
+function cipherUpperCase(char, shift) {
+	let cipher = char.charCodeAt() + shift;
+	while (cipher > 90) {
+		cipher = cipher - 91 + 65;
+	}
+	return String.fromCharCode(cipher);
+}
+
+export function caesarCipher(str, shiftFactor) {
+	return str
+		.split("")
+		.map((char) => {
+			const asciiCode = char.charCodeAt();
+			if (asciiCode > 96 && asciiCode < 123)
+				return cipherLowerCase(char, shiftFactor);
+			if (asciiCode > 64 && asciiCode < 91)
+				return cipherUpperCase(char, shiftFactor);
+			return char;
+		})
+		.join("");
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,39 @@
-import getName from "./index.js";
+import {
+	capitalize,
+	reverseString,
+	Calculator,
+	caesarCipher,
+} from "./index.js";
 
-test("Name", () => expect(getName()).toBe("vatsal"));
+// catpitalize take a string and capitalize it's first character.
+it("Capitalize first letter", () =>
+	expect(capitalize("vatsal")).toMatch("Vatsal"));
+
+it("Capitalize first letter", () =>
+	expect(capitalize("easter")).toMatch("Easter"));
+
+it("Reverse String", () => expect(reverseString("vatsal")).toMatch("lastav"));
+
+it("Reverse String", () => expect(reverseString("easter")).toMatch("retsae"));
+
+const calc = Calculator();
+it("Calculator Add", () => expect(calc.add(5, 4)).toBe(9));
+it("Calculator Add", () => expect(calc.add(9, 4)).toBe(13));
+
+it("Calculator Sub", () => expect(calc.sub(5, 4)).toBe(1));
+it("Calculator Sub", () => expect(calc.sub(9, 4)).toBe(5));
+
+it("Calculator Multiply", () => expect(calc.mul(5, 4)).toBe(20));
+it("Calculator Multiply", () => expect(calc.mul(9, 4)).toBe(36));
+
+it("Calculator Division", () => expect(calc.div(5, 4)).toBeCloseTo(1.25));
+it("Calculator Division", () => expect(calc.div(9, 4)).toBeCloseTo(2.25));
+
+it("Caesar's Cipher Repeat", () =>
+	expect(caesarCipher("xyz", 3)).toMatch(/^abc$/));
+it("Caesar's Cipher Preserve Case", () =>
+	expect(caesarCipher("tooDDler", 5)).toMatch(/^yttIIqjw$/));
+it("Caesar's Cipher Preserve Puntuations", () =>
+	expect(caesarCipher("Hello, World!", 3)).toMatch(/^Khoor, Zruog!$/));
+it("Caesar's Cipher", () =>
+	expect(caesarCipher("Hello, world!!", 50)).toMatch(/^Fcjjm, umpjb!!$/));
